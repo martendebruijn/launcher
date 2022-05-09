6821e05a9cd9b5b690a5410cb4d104901bfe2ef3
martendebruijn <marten@martendebruijn.nl>
2022-05-09 21:50:57 +0200

    ✨ feat: add configs

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
