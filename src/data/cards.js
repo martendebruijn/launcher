5a0c2818b66cf9acd34ae73dfc7fb1f0167d1b0a
martendebruijn <marten@martendebruijn.nl>
2022-05-14 01:55:37 +0200

    ✨ feat(data): add component data

---
+++ src/data/cards.js
@@ -0,0 +1,60 @@
+export default [
+  {
+    header: 'Code something',
+    color: 'gray',
+    buttons: [
+      {
+        url: 'https://github.com/martendebruijn',
+        img: {
+          url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+          alt: 'GitHub logo',
+        },
+        text: 'GitHub',
+      },
+      {
+        url: 'https://gitlab.com/martendebruijn',
+        img: {
+          url: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg',
+          alt: 'GitLab logo',
+        },
+        text: 'GitLab',
+      },
+    ],
+  },
+  {
+    header: 'Watch something',
+    color: 'red',
+    buttons: [
+      {
+        url: 'https://netflix.com/browse/',
+        img: {
+          url: 'https://cdn.svgporn.com/logos/netflix-icon.svg',
+          alt: 'YouTube logo',
+        },
+        text: 'YouTube',
+      },
+      {
+        url: 'https://youtube.com/feed/subscriptions/',
+        img: {
+          url: 'https://cdn.svgporn.com/logos/youtube-icon.svg',
+          alt: 'Netflix logo',
+        },
+        text: 'Netflix',
+      },
+    ],
+  },
+  {
+    header: 'Do some work',
+    color: 'yellow',
+    buttons: [
+      {
+        url: 'https://intranet.mozard.nl/',
+        img: {
+          url: 'https://pbs.twimg.com/profile_images/1204058513879777280/p9hxd5Ea_400x400.jpg',
+          alt: 'Mozard logo',
+        },
+        text: 'Mozard',
+      },
+    ],
+  },
+]
