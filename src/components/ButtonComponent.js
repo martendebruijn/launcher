49d4366bbcbc41def4c75b0b383bb665b2b8beda
martendebruijn <marten@martendebruijn.nl>
2022-05-14 01:49:22 +0200

    ✨ feat(ButtonComponent): add buttoncomponent

---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,74 @@
+/**
+ * ButtonComponent
+ *
+ * A button consisting of an image and text
+ *
+ * **Props**:
+ * - `url`: String - button link
+ * - `color`: String - button color
+ * - `img`: Object - image object
+ *     - `url`: String - image url
+ *     - `alt`: String - image alt text
+ * - `text`: String - button text
+ *
+ */
+export default class ButtonComponent extends HTMLElement {
+  /**
+   *
+   * @param {object} props - Component props
+   */
+  constructor(props) {
+    super()
+    this.props = props
+    const template = document.getElementById('button-component')
+    const shadow = this.attachShadow({ mode: 'open' })
+    const copy = template.content.cloneNode(true)
+    this.classList.add('inline-block')
+    const elements = {
+      link: copy.querySelector('a'),
+      iconWrapper: copy.querySelector('[data-name="iconWrapper"]'),
+      icon: copy.querySelector('img'),
+      text: copy.querySelector('[data-name="text"]'),
+    }
+
+    let classNames
+    switch (props.color) {
+      case 'red':
+        classNames = [
+          'bg-red-400',
+          'text-red-900',
+          'hover:bg-red-500',
+          'hover:text-red-100',
+        ]
+        break
+      case 'yellow':
+        classNames = [
+          'bg-yellow-400',
+          'text-yellow-900',
+          'hover:bg-yellow-500',
+          'hover:text-yellow-100',
+        ]
+        break
+      default:
+        classNames = [
+          'bg-gray-400',
+          'text-gray-900',
+          'hover:bg-gray-500',
+          'hover:text-gray-100',
+        ]
+        break
+    }
+
+    classNames.forEach((className) => {
+      elements.link.classList.add(className)
+    })
+
+    elements.link.href = props.url
+    elements.icon.src = props.img.url
+    elements.icon.alt = props.img.alt
+    elements.text.textContent = props.text
+
+    shadow.appendChild(copy)
+  }
+}
+customElements.define('button-component', ButtonComponent)
